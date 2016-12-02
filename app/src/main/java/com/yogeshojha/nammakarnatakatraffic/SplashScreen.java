a11783cf6a0b7fa88ea207411caefd5e6d5deb4e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2016-12-02 14:26:41 +0530

    Splash Edit

--- app/src/main/java/com/yogeshojha/nammakarnatakatraffic/SplashScreen.java
+++ app/src/main/java/com/yogeshojha/nammakarnatakatraffic/SplashScreen.java
@@ -5,103 +5,13 @@ package com.yogeshojha.nammakarnatakatraffic;
  */
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.PixelFormat;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 public class SplashScreen extends Activity {
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Window window = getWindow();
-        window.setFormat(PixelFormat.RGBA_8888);
-    }
     /** Called when the activity is first created. */
-    Thread splashTread;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        StartAnimations();
     }
-    private void StartAnimations() {
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
-        anim.reset();
-        RelativeLayout l=(RelativeLayout) findViewById(R.id.lin_lay);
-        l.clearAnimation();
-        l.startAnimation(anim);
-
-        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
-        anim.reset();
-        ImageView iv = (ImageView) findViewById(R.id.splash);
-        iv.clearAnimation();
-        iv.startAnimation(anim);
-
-        splashTread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int waited = 0;
-                    // Splash screen pause time
-                    while (waited < 3500) {
-                        sleep(100);
-                        waited += 100;
-                    }
-                    if(isInternetOn())
-                    {
-                        Intent intent = new Intent(SplashScreen.this,
-                                MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(intent);
-                        SplashScreen.this.finish();
-                    }
-                    else
-                    {
-                        Intent intent = new Intent(SplashScreen.this,
-                                NoInternet.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(intent);
-                        SplashScreen.this.finish();
-                    }
-                } catch (InterruptedException e) {
-                    // do nothing
-                } finally {
-                    SplashScreen.this.finish();
-                }
-
-            }
-        };
-        splashTread.start();
-
-    }
-    public final boolean isInternetOn() {
-
-        // get Connectivity Manager object to check connection
-        ConnectivityManager connec =
-                (ConnectivityManager)getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
-
-        // Check for network connections
-        if ( connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
-                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
-                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
-                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED ) {
-
-            // if connected with internet
-            return true;
-
-        } else if (
-                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
-                        connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED  ) {
-
-            //not connected
-            return false;
-        }
-        return false;
-    }
-
 }
