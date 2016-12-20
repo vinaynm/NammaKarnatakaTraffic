4d50bca1d873e7098e9fbe95d63760b646a472ba
yogesh ojha <yogesh.ojha11@gmail.com>
2016-12-20 23:59:09 +0530

    unknown commit 1

--- app/src/main/java/com/yogeshojha/nammakarnatakatraffic/VehicleRcFragment.java
+++ app/src/main/java/com/yogeshojha/nammakarnatakatraffic/VehicleRcFragment.java
@@ -1,12 +1,17 @@
 package com.yogeshojha.nammakarnatakatraffic;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
@@ -20,7 +25,8 @@ public class VehicleRcFragment extends Fragment {
 
     private AdView mAdView_rc;
     public AdRequest adRequest_rc;
-
+    public WebView webView;
+    private ProgressBar mPbar = null;
     public VehicleRcFragment() {
         // Required empty public constructor
     }
@@ -31,14 +37,67 @@ public class VehicleRcFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_vehicle_rc, container, false);
-        mAdView_rc = new AdView(getActivity());
-        mAdView_rc.setAdUnitId("ca-app-pub-9002284518905519/5700240988");
-        mAdView_rc.setAdSize(AdSize.BANNER);
-        LinearLayout layoutrc = (LinearLayout) v.findViewById(R.id.layout_admob_rc);
-        layoutrc.addView(mAdView_rc);
-        AdRequest adRequest_rc = new AdRequest.Builder().build();
-        mAdView_rc.loadAd(adRequest_rc);
+        webView = (WebView) v.findViewById(R.id.webview_rc);
+        startWebView("http://parivahan.gov.in/rcdlstatus");
         return v;
-    }
 
+    }
+    private void startWebView(String url) {
+
+        //Create new webview Client to show progress dialog
+        //When opening a url or click on link
+
+        webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+            //If you will not use this method url links are opeen in new brower not in webview
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            //Show loader on url load
+            public void onLoadResource (WebView view, String url) {
+                if (progressDialog == null) {
+                    // in standard case YourActivity.this
+                    progressDialog = new ProgressDialog(getActivity());
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+            public void onPageFinished(WebView view, String url) {
+                try{
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    }
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+
+        });
+
+        // Javascript inabled on webview
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Other webview options
+        /*
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+        webView.getSettings().setBuiltInZoomControls(true);
+        */
+
+        /*
+         String summary = "<html><body>You scored <b>192</b> points.</body></html>";
+         webview.loadData(summary, "text/html", null);
+         */
+
+        //Load url in webview
+        webView.loadUrl(url);
+
+
+    }
 }
